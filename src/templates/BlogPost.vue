4af6dea80b33ad49ca423147fca07c6c7d187af6
Surya Wiguna <suryawigunaa@gmail.com>
2019-06-27 14:53:06 +0800

    change

--- src/templates/BlogPost.vue
+++ src/templates/BlogPost.vue
@@ -56,8 +56,7 @@
         computed: {
             getImage() {
                 var firstImage = document.getElementsByClassName("g-image")[0].getAttribute("data-srcset").split(' ')[2];
-                var content = "https://suryawiguna.com"+firstImage;
-                return content;
+                return firstImage
             }
         },
         metaInfo () {
@@ -68,7 +67,7 @@
                     { name: "twitter:title", content: this.$page.blogPost.title },
                     { name: "twitter:description", content: this.$page.blogPost.description },
                     { name: "twitter:site", content: "@suryawigunaa" },
-                    { name: "twitter:image", content: this.getImage },
+                    { name: "twitter:image", content: "https://suryawiguna.com"+this.getImage },
                     { name: "twitter:creator", content: "@suryawigunaa" },
                 ],
                 script: [{ src: "https://platform.twitter.com/widgets.js", async: true }]
